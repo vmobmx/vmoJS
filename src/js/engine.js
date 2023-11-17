939545962fb1ee3e89e31cc2ff4485902adf5267
MikholapIvan <ivanmikholap6@gmail.com>
2023-11-17 21:17:24 +0300

    Change index.html, change engine.js, change testScene.js, and add style.css

--- src/js/engine.js
+++ src/js/engine.js
@@ -3,4 +3,21 @@ class Scene {
         this.gravityAcceleration = gravityAcceleration;
         this.windage = windage;
     }
+    init() {
+        this.sceneID = document.getElementById('scene');
+        this.sceneID.style.position = 'relative';
+        this.sceneID.style.width = '100%';
+        this.sceneID.style.height = '100%';
+    }
+}
+
+class Object {
+    init(isPhysical, collision, width, height, x, y) {
+        this.isPhysical = isPhysical;
+        this.collision = collision;
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+    }
 }
--- src/js/testScene.js
+++ src/js/testScene.js
@@ -1,2 +1,5 @@
 const scene = new Scene(5, 5);
+scene.init();
 
+const floor = new Object;
+floor.init(true, true, "10%", "100%", 0, 0);
