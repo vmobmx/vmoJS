c271e640202f3c6846b6195480880da290ef5818
MikholapIvan <ivanmikholap6@gmail.com>
2023-11-17 22:33:23 +0300

    Change style.css, change index.html, change engine.js, change testScene.js

--- src/js/engine.js
+++ src/js/engine.js
@@ -12,12 +12,33 @@ class Scene {
 }
 
 class Object {
-    init(isPhysical, collision, width, height, x, y) {
+    constructor(objectID, isPhysical, collision, width, height, x, y) {
+        this.objectID = objectID;
         this.isPhysical = isPhysical;
         this.collision = collision;
         this.width = width;
         this.height = height;
         this.x = x;
         this.y = y;
+    }
+
+    init(gravityAcceleration) {
+        this.objectID.style.position = 'absolute';
+        this.objectID.style.width = this.width;
+        this.objectID.style.height = this.height;
+        this.objectID.style.left = this.x;
+        this.objectID.style.top = this.y;   
+    }
+
+    giveVerticalAcceleration(acceleration) {
+        this.verticalAcceleration += acceleration;
+    }
+
+    giveGorizontalAcceleration(acceleration) {
+        this.gorizontalAcceleration = acceleration;
+    }
+
+    move() {
+        this.objectID.style.top += this.verticalAcceleration;
     }
 }
--- src/js/testScene.js
+++ src/js/testScene.js
@@ -1,5 +1,19 @@
 const scene = new Scene(5, 5);
 scene.init();
 
-const floor = new Object;
-floor.init(true, true, "10%", "100%", 0, 0);
+const floorID = document.getElementById('floor');
+const boxID = document.getElementById('box');
+
+const floor = new Object(floorID, false, true, '100%', '5%', '0px', '915px');
+const box = new Object(boxID, true, true, '100px', '100px', '100px', '100px');
+
+floor.init();
+box.init();
+
+box.giveVerticalAcceleration(5);
+
+// loop
+
+setInterval(() => {
+    box.move();
+}, 150);
